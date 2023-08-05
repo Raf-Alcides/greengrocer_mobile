2d7cf9a75b255c5ba35cbe9f664d32493ef21fc7
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-08-05 01:07:17 -0300

    Criando HttpManager

---
+++ lib/app/core/ui/services/http_manager.dart
@@ -0,0 +1,43 @@
+import 'dart:core';
+import 'package:dio/dio.dart';
+
+abstract class HttpMethods {
+  static const String post = 'POST';
+  static const String get = 'GET';
+  static const String delete = 'DELETE';
+  static const String put = 'PUT';
+  static const String patch = 'PATCH';
+}
+
+class HttpManager {
+  Future<Map> restRequest({
+    required String url,
+    required String method,
+    Map? headers,
+    Map? body,
+  }) async {
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'X-Parse-REST-API-Key': '2kahi62fkWePLWAwC7k8aMrtQkobogcgkruMxbeB',
+        'X-Parse-Application-Id': 'wK7GcEjr2V4br5q5mlR1kybQ5dvxMFDX0qtE1d6Y'
+      });
+    Dio dio = Dio();
+    try{
+      Response response = await dio.request(
+      url,
+      options: Options(
+        method: method,
+        headers: defaultHeaders,
+      ),
+      data: body,
+    );
+    return response.data;
+    } on DioException catch(error) {
+      return error.response!.data ?? {};
+    } catch(error) {
+      return {};
+    }
+  }
+}
