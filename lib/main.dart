f9022e6a228d385c8d6d7f15ce94fb509e7a4f37
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-08-04 22:40:51 -0300

    Validando dados dos campos

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:greengrocer_mobile/app/pages/auth/sign_in_screen.dart';
 import 'package:greengrocer_mobile/app/core/ui/ui_config.dart';
 import 'package:greengrocer_mobile/app/pages_routes/app_pages.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         initialRoute: PagesRoutes.signinRoute,
         getPages: AppPages.pages,
         debugShowCheckedModeBanner:  false,
-        home: const SignInScreen()
+        
       ),
     );
   }
